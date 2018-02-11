03c806456ebb3155dc5e047318bdda0365ae2941
rfukutome <rfukutome@gmail.com>
2018-02-10 18:39:09 -0800

    Adding unity test scene, as well as basic camera look functionality.

---
+++ Assets/Scripts/LookX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // Rotation
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += mouseX;
+        transform.localEulerAngles = newRotation;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float _speed;
+    private CharacterController _controller;
+    private float _gravity = 9.81f;
+
+	// Use this for initialization
+	void Start () {
+        _controller = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Movement
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        direction.y -= _gravity;
+
+        direction = transform.transform.TransformDirection(direction);
+        _controller.Move(_speed*direction*Time.deltaTime);
+	}
+}
