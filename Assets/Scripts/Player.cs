604adf87ea5d0310fc80eb266589b11c0bc2493f
rfukutome <rfukutome@gmail.com>
2018-02-10 19:51:28 -0800

    Adding gun sounds and shooting mechanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,76 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] private GameObject _muzzleFlash;
     [SerializeField] private float _speed;
+    [SerializeField] private GameObject _hitMarker;
+    [SerializeField] private AudioSource _gunSound;
+    [SerializeField] private UIManager _uiManager;
     private CharacterController _controller;
     private float _gravity = 9.81f;
+    [SerializeField] private bool _reloading;
 
+    [SerializeField]
+    private int currentAmmo;
+    private int maxAmmo = 50;
 	// Use this for initialization
 	void Start () {
-        _controller = GetComponent<CharacterController>();	
+        currentAmmo = maxAmmo;
+        _controller = GetComponent<CharacterController>();
+        _uiManager = GameObject.FindObjectOfType<UIManager>();
+        _uiManager.UpdateAmmo(currentAmmo);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _reloading = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetButton("Fire1") && currentAmmo > 0)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _gunSound.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !_reloading)
+        {
+            StartCoroutine(Reload());
+            _reloading = true;
+        }
+        Movement();
+	}
+
+    void Shoot()
+    {
+        currentAmmo--;
+        _uiManager.UpdateAmmo(currentAmmo);
+        if (!_gunSound.isPlaying)
+        {
+            _gunSound.Play();
+        }
+        _muzzleFlash.SetActive(true);
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            Debug.Log("Hit!" + hitInfo.transform.name);
+            GameObject hitMarker = Instantiate(_hitMarker, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(hitMarker, 0.3f);
+        }
+    }
+    void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -23,6 +82,14 @@ public class Player : MonoBehaviour {
         direction.y -= _gravity;
 
         direction = transform.transform.TransformDirection(direction);
-        _controller.Move(_speed*direction*Time.deltaTime);
-	}
+        _controller.Move(_speed * direction * Time.deltaTime);
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _reloading = false;
+        currentAmmo = maxAmmo;
+        _uiManager.UpdateAmmo(currentAmmo);
+    }
 }
